41c6cf204a35c16811353ad5d723a5593a330222
wenguoli <15160333779@163.com>
2016-10-22 22:38:47 +0800

    Initial Commit

---
+++ GLAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLAlertView
+//
+//  Created by 温国力 on 16/10/22.
+//  Copyright © 2016年 wgl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLAlertView
+//
+//  Created by 温国力 on 16/10/22.
+//  Copyright © 2016年 wgl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
